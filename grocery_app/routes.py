7898bf3e614e42274335130c5e81f5a3fc821898
nekacurry <anneka498@gmail.com>
2021-04-26 13:46:56 -0500

    Update routes.py

--- grocery_app/routes.py
+++ grocery_app/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, render_template, redirect, url_for, flash
 from datetime import date, datetime
 from grocery_app.models import GroceryStore, GroceryItem
+from grocery_app.forms import GroceryItemForm, GroceryStoreForm
 
 # Import app and db from events_app package so that we can run app
 from grocery_app import app, db
@@ -20,14 +21,24 @@ def homepage():
 @main.route('/new_store', methods=['GET', 'POST'])
 def new_store():
     # TODO: Create a GroceryStoreForm
+    form = GroceryStoreForm()
 
     # TODO: If form was submitted and was valid:
     # - create a new GroceryStore object and save it to the database,
     # - flash a success message, and
     # - redirect the user to the store detail page.
+    if form.validate_on_submit():
+      new_store = GroceryStore(
+        title = form.title.data,
+        address = form.address.data
+      )
+      db.session.add(new_store)
+      db.session.commit()
+      flash('New store successfully created!')
+      return redirect(url_for('main.store_detail', store_id=new_store.id))
 
     # TODO: Send the form to the template and use it to render the form fields
-    return render_template('new_store.html')
+    return render_template('new_store.html', form=form)
 
 @main.route('/new_item', methods=['GET', 'POST'])
 def new_item():
