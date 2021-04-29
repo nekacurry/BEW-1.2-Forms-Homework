c45f2554b527e1833e9ea27039bfb55a7999f14c
nekacurry <anneka498@gmail.com>
2021-04-29 16:57:53 -0500

    Update __init__.py

--- grocery_app/__init__.py
+++ grocery_app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from grocery_app.config import Config
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 import os
 
 app = Flask(__name__)
@@ -8,6 +10,18 @@ app.config.from_object(Config)
 app.secret_key = os.urandom(24)
 
 db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+
+from .models import User
+
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(user_id)
+
+bcrypt = Bcrypt(app)
 
 from grocery_app.routes import main
 
