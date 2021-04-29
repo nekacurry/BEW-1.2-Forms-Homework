0c69ef848bbb6cccfd5ba71314629efdabcb175d
nekacurry <anneka498@gmail.com>
2021-04-29 17:47:25 -0500

    Update routes.py

--- grocery_app/routes.py
+++ grocery_app/routes.py
@@ -127,6 +127,21 @@ def item_detail(item_id):
     # TODO: Send the form to the template and use it to render the form fields
     item = GroceryItem.query.get(item_id)
     return render_template('item_detail.html', item=item, form=form)
+  
+@main.route('/add_to_shopping_list/<item_id>', methods=['POST'])
+@login_required
+def add_to_shopping_list(item_id):
+    item = db.session.query(GroceryItem).get(item_id)
+    if item != None:
+        current_user.shopping_list_items.append(item)
+        db.session.commit()
+    return redirect(url_for('main.shopping_list'))
+
+@main.route('/shopping_list')
+@login_required
+def shopping_list():
+    return render_template('shopping_cart.html', current_user=current_user)
+
 
 # Authentication
 
