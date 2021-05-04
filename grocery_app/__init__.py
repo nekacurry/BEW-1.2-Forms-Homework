21a61d8aabf900745aeca0e0442eb62e6a2865a6
nekacurry <anneka498@gmail.com>
2021-05-04 16:30:27 -0500

    fixed auth

--- grocery_app/__init__.py
+++ grocery_app/__init__.py
@@ -23,9 +23,10 @@ def load_user(user_id):
 
 bcrypt = Bcrypt(app)
 
-from grocery_app.routes import main
+from grocery_app.routes import main, auth
 
 app.register_blueprint(main)
+app.register_blueprint(auth)
 
 with app.app_context():
     db.create_all()
